8271501837a187d7bb32c93b1cd2564b012ec859
Thoth Ky <28805113+thoth-ky@users.noreply.github.com>
2020-06-18 04:45:31 +0300

    override user creation via serializer

--- user/serializers.py
+++ user/serializers.py
@@ -1,37 +1,44 @@
-from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from django.contrib.auth import authenticate
-
+from user.models import User
 class UserSerializer(serializers.ModelSerializer):
   class Meta:
-      model = get_user_model()
-      fields = ('username', 'first_name', 'last_name', 'email', 'password', 'last_login', 'is_superuser', 'is_active')
-      read_only_fields = ('last_login', 'is_superuser', 'is_active')
-      extra_kwargs = {
-          'password': {'write_only': True}
-      }
+    model = User
+    fields = ('username', 'first_name', 'last_name', 'email', 'password', 'last_login', 'is_superuser', 'is_active')
+    read_only_fields = ('last_login', 'is_superuser', 'is_active')
+    extra_kwargs = {
+        'password': {'write_only': True}
+    }
+  
+  def create(self, validated_data):
+    user = User(**validated_data)
+    user.set_password(validated_data["password"])
+    user.save()
+    return user
+  
+
 
 class AuthSerializer(serializers.Serializer):
-    username = serializers.CharField()
-    password = serializers.CharField()
+  username = serializers.CharField()
+  password = serializers.CharField()
 
-    def validate(self, attrs):
-        username = attrs.get('username')
-        password = attrs.get('password')
+  def validate(self, attrs):
+    username = attrs.get('username')
+    password = attrs.get('password')
 
-        if username and password:
-            user = authenticate(username=username, password=password)
+    if username and password:
+      user = authenticate(username=username, password=password)
 
-            if user:
-                if not user.is_active:
-                    msg = 'User account is disabled.'
-                    raise serializers.ValidationError(msg, code='authorization')
-            else:
-                msg = 'Unable to log in with provided credentials.'
-                raise serializers.ValidationError(msg, code='authorization')
+      if user:
+        if not user.is_active:
+          msg = 'User account is disabled.'
+          raise serializers.ValidationError(msg, code='authorization')
+      else:
+        msg = 'Unable to log in with provided credentials.'
+        raise serializers.ValidationError(msg, code='authorization')
 
-        else:
-            msg = 'Must include "username" and "password"'
-            raise serializers.ValidationError(msg, code='authorization')
-        attrs['user'] = user
-        return attrs
+    else:
+      msg = 'Must include "username" and "password"'
+      raise serializers.ValidationError(msg, code='authorization')
+    attrs['user'] = user
+    return attrs
